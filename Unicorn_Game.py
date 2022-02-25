363e2578dfed75c302fc5c9499674ffed5c2d731
rolandCameron <99928788+rolandCameron@users.noreply.github.com>
2022-02-25 13:08:42 +1300

    First Backup
    Contains the decomposition of the program

---
+++ Unicorn_Game.py
@@ -0,0 +1,28 @@
+#IMPORTS
+
+#VARIABLES
+
+#FUNCTIONS
+
+#MAIN
+
+'''
+1. Ask how much the user is going to deposit.
+a. Never let the user spend more than $10 (across all rounds)
+
+2. Ask how much the user wants to pay for this round(p), min of $1
+
+3. Generate a random token
+
+4. Depending on the token, change their balance appropriately.
+a. If jackpot(Unicorn) and 5 times how much they paid for the round
+b. If neutral(Horse or zebra) add half as much as they paid for the round
+c. If a loss(Donkey) pay nothing to the player
+
+5. Show the user how much they earned
+
+6. Ask if they want to play again
+'''
+
+
+
