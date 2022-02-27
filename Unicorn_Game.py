197af03b102a6073237c79b2ffefcf26fb68efc7
rolandCameron <99928788+rolandCameron@users.noreply.github.com>
2022-02-28 11:31:56 +1300

    Second edition
    Added the first step, the deposit system. Has error proofing and stores the balance in a variable.

--- Unicorn_Game.py
+++ Unicorn_Game.py
@@ -1,15 +1,49 @@
 #IMPORTS
 
 #VARIABLES
+totalDeposited = 0
+balance = 0
 
 #FUNCTIONS
+def Is_Int(string):
+    try:
+        string = int(string)
+        return True
+    except:
+        return False
 
 #MAIN
 
-'''
-1. Ask how much the user is going to deposit.
-a. Never let the user spend more than $10 (across all rounds)
+while True:
+    if totalDeposited < 10:
+        gettingDeposit = True
+    else:
+        gettingDeposit = False
 
+    while gettingDeposit:
+        if input("Would you like to add more money to your account? (y/n) ".lower()) == "y":
+            while gettingDeposit:
+                deposit = input("How much would you like to deposit? (Enter a whole $ value) ")
+                if Is_Int(deposit):
+                    deposit = int(deposit)
+                    if not totalDeposited + deposit > 10:
+                        balance += deposit
+                        totalDeposited += deposit
+
+                        print("Deposit made, your balance is now ${}.".format(balance))
+
+                        gettingDeposit = False
+                    else:
+                        print("Depositing that much money would result in you exceeding out $10 limit on spending. Please deposit less money.")
+
+                else:
+                    print("Make sure you are entering a whole dollar value, no decimal places, and that there are only numbers. ")
+                    continue
+        else:
+            gettingDeposit = False
+            continue
+
+'''
 2. Ask how much the user wants to pay for this round(p), min of $1
 
 3. Generate a random token
